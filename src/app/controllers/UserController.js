f2156e973da2fcbf3ae049bc32a07fb6b8c32bb8
Seu Nome <seuemail@example.com>
2024-06-02 11:49:15 -0300

    adicionado as validações de  email e de senha na aplicação,e corrigido o bug de postbird

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -5,24 +5,50 @@ show     => Listar apenas um
 update   => Atualizar
 delete   => deletar 
 */
-import { v4 } from 'uuid'
-import User from '../models/User'
+import { v4 } from "uuid";
+import User from "../models/User";
+import * as Yup from "yup";
 
-class UserController{
- async store(request,response){
+class UserController {
+  async store(request, response) {
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      email: Yup.string().email().required(),
+      password_hash: Yup.string().required().min(6),
+      admin: Yup.boolean(),
+    });
 
-        const { name, email, password_hash, admin,} = request.body
- 
-        const user = await User.create({
-        id: v4(),
-        name,
-        email,
-        password_hash,
-        admin,
-    })
+    // if (!(await schema.isValid(request.body))) {
+    //   return response
+    //     .status(400)
+    //     .json({ error: "make sure your data is correct" });
+    // }
 
-       return response.json(user)
+    try {
+      await schema.validateSync(request.body, { abortEarly: false });
+    } catch (err) {
+      return response.status(400).json({ error: err.errors });
     }
+
+    const { name, email, password_hash, admin } = request.body;
+
+    const userExists = await User.findOne({
+      where: { email },
+    });
+    if (userExists) {
+      return response.status(400).json({ error: "User already exists" });
+    }
+
+    const user = await User.create({
+      id: v4(),
+      name,
+      email,
+      password_hash,
+      admin,
+    });
+
+    return response.json(user);
+  }
 }
 
-export default new UserController()
+export default new UserController();
