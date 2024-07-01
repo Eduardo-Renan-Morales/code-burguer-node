9203a63b27169072619e8f8ab83e68fb68829460
Seu Nome <seuemail@example.com>
2024-07-01 20:30:19 -0300

    terminado a partedo back-end

---
+++ src/database/migrations/20240628222139-delete-category-path.js
@@ -0,0 +1,17 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.removeColumn("categories", "path");
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.createColumn("categories", "path", {
+      category: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+    });
+  },
+};
