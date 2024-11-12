e8c4dddadbebaa431d8f18808deca7b7a99ad39d
Seu Nome <seuemail@example.com>
2024-11-11 22:04:17 -0300

    removudo file name

--- src/app.js
+++ src/app.js
@@ -5,11 +5,6 @@ import routes from "./routes.js";
 
 import "./database/index.js";
 
-import path from 'path';
-import { fileURLToPath } from 'url';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 class App {
   constructor() {
     this.app = express();
--- src/config/database.js
+++ src/config/database.js
@@ -1,10 +1,10 @@
 
-// src/config/database.js
+
 export default {
   dialect: "postgres",
   url: "postgresql://postgres:AllVUEqxjgIJdYBzkDfonJQdqeVymsoW@autorack.proxy.rlwy.net:35953/railway",
   define: {
-    timestamps: true, // Corrigido para "timestamps"
+    timespamps: true,
     underscored: true,
     underscoredAll: true,
   },
